a44a06a05d02f24c3a9a3e72f229c82e975b67f9
joseph085 <126801512+joseph085@users.noreply.github.com>
2023-05-05 18:01:31 +0400

    Inserting arrays and how they work in RAM

---
+++ C#17_lessons/More_on_ARRAYS_/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace More_on_ARRAYS_
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //int number = 4;
+
+        //    //Multiply(number);
+
+        //    //Console.WriteLine(number);
+
+        //}
+        //static void Multiply(int yusif)
+        //{
+        //    yusif = yusif * 2;
+
+        //    Console.WriteLine(yusif);
+        //}
+
+        //static void Main(string[] args) //entry point 
+        //{
+        //    int[] numbers = { 5, 7, 9, 11, 6, 8, 13 };
+
+        //    Console.WriteLine(numbers[0]);
+
+        //    MultiplyBy(numbers[0]);
+        //    MultiplyBy(numbers[2]);
+        //    MultiplyBy(numbers[3]);
+        //    MultiplyBy(numbers[4]);
+        //    MultiplyBy(numbers[5]);
+
+        //    Console.WriteLine(numbers[0]);
+        //}
+        //static void MultiplyBy(int numbers)
+        //{
+        //    numbers += 2;
+
+        //    Console.WriteLine(numbers);
+
+        //}
+        //static void MultiplyByElement()
+        //{
+        //    Console.WriteLine();
+
+        //}
+
+
+    }
+}
